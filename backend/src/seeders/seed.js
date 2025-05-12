ab3f1698c1481af8c91b789d146d32bb62b83225
HLF <106381172+hoolyfook@users.noreply.github.com>
2025-05-12 19:04:52 +0700

    chore: seed account for dev

--- backend/src/seeders/seed.js
+++ backend/src/seeders/seed.js
@@ -227,6 +227,39 @@ module.exports = {
         ]);
         await queryInterface.bulkInsert('Users', [
             {
+                hoTen: 'Admin',
+                gioiTinh: 'Female',
+                ngaySinh: '2005-04-01', // 2005 -> 15 years old in 2020
+                diaChi: 'Hanoi, Vietnam',
+                email: 'ad@example.com',
+                mk: 'password123',
+                groupUserId: 1, // Reference a group from GroupUsers
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                hoTen: 'GiangVien',
+                gioiTinh: 'Female',
+                ngaySinh: '2005-04-01', // 2005 -> 15 years old in 2020
+                diaChi: 'Hanoi, Vietnam',
+                email: 'gv@example.com',
+                mk: 'password123',
+                groupUserId: 2, // Reference a group from GroupUsers
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                hoTen: 'User',
+                gioiTinh: 'Female',
+                ngaySinh: '2005-04-01', // 2005 -> 15 years old in 2020
+                diaChi: 'Hanoi, Vietnam',
+                email: 'user@example.com',
+                mk: 'password123',
+                groupUserId: 3, // Reference a group from GroupUsers
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
                 hoTen: 'Nguyễn Thị A',
                 gioiTinh: 'Female',
                 ngaySinh: '2005-04-01', // 2005 -> 15 years old in 2020
